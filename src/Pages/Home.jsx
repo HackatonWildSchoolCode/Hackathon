dac6788a82410e7c642b83f95f23edd5ff3f8a48
Amandine Leporace <leporace.amandine@gmail.com>
2022-11-24 16:53:47 +0100

    sauvegarde amandine

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import "../Styles/Home.css"
-
+import React from "react";
+import Space from "../assets/Space.mp4";
+import "../Styles/Home.css";
 
 const Home = () => {
-    return (
-        <div className='homepage'>
-            <h1>Voyage & temps</h1>
-        </div>
-    );
+  return (
+    <>
+      <div className="space">
+        <video src={Space} autoPlay loop muted />
+      </div>
+      <div className="slogan">
+        <h3>Keep the lights on in the dark</h3>
+      </div>
+    </>
+  );
 };
 
 export default Home;
