cc7c1360a37cd1f8c443446b07622f49fe47d8e8
Amandine Leporace <leporace.amandine@gmail.com>
2022-11-25 02:57:45 +0100

    Final bug fix Hackaton

--- src/Transport/Transport.jsx
+++ src/Transport/Transport.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "../Styles/Settings.css";
 import "../Styles/Transport.css";
 import data from "./data.js";
+import logo from "../assets/Logo.png";
+import Zoom from "react-reveal/Zoom";
 
 const Transport = () => {
   const [selectedTechnology, setSelectedTechnology] = useState(
@@ -15,37 +17,48 @@ const Transport = () => {
   };
 
   return (
-    <div className="page-transport">
-      <div className="content">
-        <h5 className="title">
-          <span>05</span> SPACE LAUNCH 101
-        </h5>
-        <div className="transport-navigation">
-          <nav>
-            <ul className="nav-button-list">
-              {data.technologies.map((item, index) => (
-                <li key={index}>
-                  <button
-                    className={`nav-button ${
-                      item.id === selectedTechnology ? "active" : ""
-                    }`}
-                    onClick={() => handleNavigation(item.id)}
-                  >
-                    {item.id}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <div className="text-container">
-            <div className="navigation-item-title">
-              <h3>{technologyInfo.name}</h3>
-            </div>
-            <div className="navigation-item-description">
-              <p className="body-text">{technologyInfo.description}</p>
-            </div>
+    <div>
+      <img className="logo" src={logo} />
+      <div className="page-transport">
+        <div className="content">
+          <h5 className="title">
+            <Zoom>
+              <span>05</span> SPACE LAUNCH 101
+            </Zoom>
+          </h5>
+          <div className="transport-navigation">
+            <nav>
+              <ul className="nav-button-list">
+                {data.technologies.map((item, index) => (
+                  <li key={index}>
+                    <button
+                      className={`nav-button ${
+                        item.id === selectedTechnology ? "active" : ""
+                      }`}
+                      onClick={() => handleNavigation(item.id)}
+                    >
+                      {item.id}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <Zoom>
+              <div className="text-container">
+                <div className="navigation-item-title">
+                  <h3>{technologyInfo.name}</h3>
+                </div>
+                <div className="navigation-item-description">
+                  <p className="body-text">{technologyInfo.description}</p>
+                </div>
+              </div>
+            </Zoom>
+            <img
+              className="technology-image"
+              src={technologyInfo.images.portrait}
+              alt="technology"
+            />
           </div>
-          <img className="technology-image" src={technologyInfo.images.portrait} alt="technology" />
         </div>
       </div>
     </div>
